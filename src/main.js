3442810c22c964e105812ab598a4900a0e71de5c
mihailocv <127780035+mihailocv@users.noreply.github.com>
2025-01-30 18:03:23 +0100

    commit work day 1

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const time = document.querySelector('#time');
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const apiKey = import.meta.env.VITE_API_KEY
+
+
+function setTime(){
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = formatClock(m);
+  
+  time.innerHTML = h + ":" + m;
+  setTimeout(setTime, 1000);
+}
+
+function formatClock(i){
+  if(i<10)
+  {
+    i = "0"+i;
+  }
+  return i;
+}
+
+function setDate(){
+  let currentDate = new Date().toLocaleDateString('sr-RS');
+  date.innerHTML = currentDate;
+}
+
+function setTemp(){
+  fetch(`http://api.openweathermap.org/data/2.5/weather?id=3194360&units=metric&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(displayData)
+  .catch(err => console.log('Gresk sa vremenom'));
+}
+
+const displayData=(weather)=>{
+  temp.innerHTML=`${Math.round(weather.main.temp)}&degC`
+}
+
+setTime();
+setDate();
+setTemp();
